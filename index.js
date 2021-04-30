69160cce59f4f73167f814f29e0e081b4ac74447
Programming Wheels <programmingwheels@gmail.com>
2021-04-30 14:38:23 +0530

    Registration API

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express")
 
 const app = express();
 
+app.get("/register",(req,res)=>{
+   res.json({message:"This is registration"})
+})
+
 app.listen(3000,()=>{
     console.log(`running on port 3000`)
 })
